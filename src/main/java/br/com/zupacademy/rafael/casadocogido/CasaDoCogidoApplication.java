ba7ae71b42d59a5d067d4bd8ba142745b5ad7f45
Rafael Couto Zup <rafael.couto@zup.com.br>
2021-04-28 17:45:23 -0300

    setup projeto casa do código

---
+++ src/main/java/br/com/zupacademy/rafael/casadocogido/CasaDoCogidoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.rafael.casadocogido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaDoCogidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaDoCogidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zupacademy/rafael/casadocogido/CasaDoCogidoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.rafael.casadocogido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaDoCogidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
